63ceb9cf66872eec366c7efa8673105af8e1fa28
Yamen-OoO <yamenlazylazy@gmail.com>
2024-10-29 18:47:39 +0300

    new changes

--- js/index.js
+++ js/index.js
@@ -6,54 +6,73 @@ let tringle3 = document.querySelector("#tringle3")
 let pone = document.querySelector('li.one')
 let ptow = document.querySelector('li.tow')
 let pthree = document.querySelector('li.three')
+let pfour = document.querySelector('li.four')
 
 
 
-pone.addEventListener("mouseover" , function(){
+pone.addEventListener("mouseover", function () {
     tringle1.style.top = "20%"
     tringle2.style.top = "10%"
     tringle3.style.top = "40%"
 })
-pone.addEventListener("mouseleave" , function(){
+pone.addEventListener("mouseleave", function () {
     tringle1.style.top = "10%"
     tringle2.style.top = "30%"
     tringle3.style.top = "20%"
 })
 
 
-ptow.addEventListener("mouseover" , function(){
+ptow.addEventListener("mouseover", function () {
     tringle1.style.top = "20%"
     tringle2.style.top = "35%"
     tringle3.style.top = "10%"
 })
-ptow.addEventListener("mouseleave" , function(){
+ptow.addEventListener("mouseleave", function () {
     tringle1.style.top = "10%"
     tringle2.style.top = "30%"
     tringle3.style.top = "20%"
 })
 
 
-pthree.addEventListener("mouseover" , function(){
+pthree.addEventListener("mouseover", function () {
     tringle1.style.top = "18%"
     tringle2.style.top = "4%"
     tringle3.style.top = "30%"
 })
-pthree.addEventListener("mouseleave" , function(){
+pthree.addEventListener("mouseleave", function () {
     tringle1.style.top = "10%"
     tringle2.style.top = "30%"
     tringle3.style.top = "20%"
 })
 
 
-// pfour.addEventListener("mouseover" , function(){
-//     tringle1.style.top = "16%"
-//     tringle2.style.top = "00%"
-//     tringle3.style.top = "40%"
-// })
-// pfour.addEventListener("mouseleave" , function(){
-//     tringle1.style.top = "10%"
-//     tringle2.style.top = "30%"
-//     tringle3.style.top = "20%"
-// })
+pfour.addEventListener("mouseover" , function(){
+    tringle1.style.top = "30%"
+    tringle2.style.top = "0%"
+    tringle3.style.top = "10%"
+})
+pfour.addEventListener("mouseleave" , function(){
+    tringle1.style.top = "10%"
+    tringle2.style.top = "30%"
+    tringle3.style.top = "20%"
+})
+
+
+
+window.addEventListener("load", () => {
+
+    let openUpdatesButton = document.querySelector(".open-updates")
+    console.log(openUpdatesButton)
+
+    setTimeout(() => {
+        openUpdatesButton.click()
+    }, 7000);
+})
+
+
+
+
+
+
 
 
